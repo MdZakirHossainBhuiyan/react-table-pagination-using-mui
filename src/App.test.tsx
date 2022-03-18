467ca8c4187549ae22c469932e288391e32cc29f
unknown <zakirhossaintuhin@gmail.com>
2022-03-19 00:09:26 +0600

    jest unit test including

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from './Components/Home/Home';
+import Details from './Components/Details/Details';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />)
+  });
+
+  it("renders Home component without crashing", () => {
+    shallow(<Home />)
+  });
+
+  it("renders Home component header without crashing", () => {
+    const wrapper = shallow(<Home />);
+    const header = (<h1 style={{"textAlign": "center"}}>Post list</h1>);
+    expect(wrapper.contains(header)).toEqual(true);
+  });
+
+  it("renders CountryInfo component without crashing", () => {
+    shallow(<Details />)
+  });
+})
--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ import Details from './Components/Details/Details';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:post" element={<Details />} />
-      
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
     </Routes>
   );
 }
